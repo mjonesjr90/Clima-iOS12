7ceb4588307a1dddf4361c71747413418b0054fa
Malcom <mjonesjr90@gmail.com>
2019-01-26 14:54:16 -0500

    first commit

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -18,7 +22,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -32,9 +36,13 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters //the more accurate, the more battery hungry
         
+        //Ask for permission
+        locationManager.requestWhenInUseAuthorization()
         
+        locationManager.startUpdatingLocation() //asynch method that works in background
     }
     
     
@@ -43,7 +51,20 @@ class WeatherViewController: UIViewController {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
+    func getWeatherData(url: String, parameters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                print("Successful API call")
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                // Let user know there was a failure
+            }
+        }
+        
+    }
 
     
     
@@ -76,11 +97,29 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Location is saved in array of CLLocation objects called locations
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 { // once the location is accurate, stop trying to find it - to save battery
+            locationManager.stopUpdatingLocation()
+            
+            print("lon = \(location.coordinate.longitude), lat = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params: [String: String] = ["lat": latitude, "lon": longitude, "appid": APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        // let the user know location is unavailable
+    }
     
     
 
